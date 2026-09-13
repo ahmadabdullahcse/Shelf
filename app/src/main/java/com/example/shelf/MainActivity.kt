546c61725c2bcf0212e86af9a0e072f46f84a2e7
ahmadabdullahcse <ahmadabdullahcse@gmail.com>
2026-09-13 17:36:49 +0600

    Created function formatDuration

--- app/src/main/java/com/example/shelf/MainActivity.kt
+++ app/src/main/java/com/example/shelf/MainActivity.kt
@@ -33,7 +33,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = """
+        0 min = ${formatDuration(0)}
+        60 min = ${formatDuration(60)}
+        75 min = ${formatDuration(75)}
+        120 min = ${formatDuration(120)}
+        711 min = ${formatDuration(711)}
+        """.trimIndent(),
         modifier = modifier
     )
 }
@@ -44,4 +50,8 @@ fun GreetingPreview() {
     ShelfTheme {
         Greeting("Abdullah")
     }
+}
+
+fun formatDuration(minutes: Int): String {
+    return "${minutes/60}h ${minutes%60}m"
 }
